614e301c803785e3f1b09378148b6a66e9394e74
djp <dominic.page@hp.com>
2015-03-03 16:03:24 +0000

    issue with url

--- rest.rb
+++ rest.rb
@@ -1,12 +1,33 @@
 require "net/http"
+require 'json'
 
-http = Net::HTTP.new("https://api.idolondemand.com")
-request = Net::HTTP::Get.new("/1/api/sync/recognizespeech/v1")
+uri = URI("https://api.idolondemand.com/1/api/async/recognizespeech/v1")
+params = { :url => "http://downloads.bbc.co.uk/podcasts/radio4/thought/thought_20150302-1134a.mp3", :language => "en-GB", :apikey => "3e25f657-9700-4e26-99ee-4c2544536e7a"}
+uri.query = URI.encode_www_form(params)
 
-request.set_form_data({"v1[url]" => "http%3A%2F%2Fdownloads.bbc.co.uk%2Fpodcasts%2Fradio4%2Fthought%2Fthought_20150302-1134a.mp3"})
-request.set_form_data({"v1[language]" => "en-GB"})
-request.set_form_data({"v1[apikey]" => "3e25f657-9700-4e26-99ee-4c2544536e7a"})
+res = Net::HTTP.get_response(uri)
+obj = JSON.parse(res.body)
 
-response = http.request(request)
+# puts res.body if res.is_a?(Net::HTTPSuccess)
+puts obj['jobID']
 
-print request
+uri = URI("https://api.idolondemand.com/1/job/status/" + obj['jobID'])
+params = {:apikey => "3e25f657-9700-4e26-99ee-4c2544536e7a"}
+uri.query = URI.encode_www_form(params)
+
+res = Net::HTTP.get_response(uri)
+obj = JSON.parse(res.body)
+
+while obj['status'] == 'queued' do 
+   res = Net::HTTP.get_response(uri)
+   obj = JSON.parse(res.body)
+  sleep(5)
+end
+
+uri = URI("https://api.idolondemand.com/1/job/results/" + obj['jobID'])
+params = {:apikey => "3e25f657-9700-4e26-99ee-4c2544536e7a"}
+uri.query = URI.encode_www_form(params)
+
+res = Net::HTTP.get_response(uri)
+puts res.body
+
