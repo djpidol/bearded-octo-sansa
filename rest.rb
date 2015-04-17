4735f7a43e96a44caa223ee2f17478a8b5a96cc3
djp <dominic.page@hp.com>
2015-04-17 12:45:26 +0000

    api key to local

--- rest.rb
+++ rest.rb
@@ -1,13 +1,14 @@
 require "net/http"
 require 'json'
 
+api_key= "58fa4fef-c49a-464e-82be-a30aad009099"
 uri = URI("https://api.idolondemand.com/1/api/async/recognizespeech/v1")
-#params = { :url => "http://downloads.bbc.co.uk/podcasts/radio4/thought/thought_20150302-1134a.mp3", :language => "en-GB", :apikey => "3e25f657-9700-4e26-99ee-4c2544536e7a"}
-#params = { :file => "thought.mp3", :language => "en-GB", :apikey => "58fa4fef-c49a-464e-82be-a30aad009099", :interval => "10"}
+#params = { :url => "http://downloads.bbc.co.uk/podcasts/radio4/thought/thought_20150302-1134a.mp3", :language => "en-GB", :apikey => api_key}
+#params = { :file => "thought.mp3", :language => "en-GB", :apikey => api_key, :interval => "10"}
 #uri.query = URI.encode_www_form(params)
 
-#res = Net::HTTP.post_form(uri, 'file' => "thought.mp3", 'language' => "en-GB", 'apikey' => "58fa4fef-c49a-464e-82be-a30aad009099", 'interval' => "10")
-res = Net::HTTP.post_form(uri, 'reference' => "a3e0121e-cc8a-4d3b-9f52-0b012518ecdc", 'language' => "en-GB", 'apikey' => "58fa4fef-c49a-464e-82be-a30aad009099", 'interval' => "10")
+#res = Net::HTTP.post_form(uri, 'file' => "thought.mp3", 'language' => "en-GB", 'apikey' => api_key, 'interval' => "10")
+res = Net::HTTP.post_form(uri, 'reference' => "a3e0121e-cc8a-4d3b-9f52-0b012518ecdc", 'language' => "en-GB", 'apikey' => api_key, 'interval' => "10")
 
 #res = Net::HTTP.get_response(uri)
 obj = JSON.parse(res.body)
@@ -16,7 +17,7 @@ obj = JSON.parse(res.body)
 puts obj['jobID']
 
 uri = URI("https://api.idolondemand.com/1/job/status/" + obj['jobID'])
-params = {:apikey => "58fa4fef-c49a-464e-82be-a30aad009099"}
+params = {:apikey => api_key}
 uri.query = URI.encode_www_form(params)
 
 res = Net::HTTP.get_response(uri)
@@ -29,7 +30,7 @@ while obj['status'] == 'queued' do
 end
 
 uri = URI("https://api.idolondemand.com/1/job/result/" + obj['jobID'])
-params = {:apikey => "58fa4fef-c49a-464e-82be-a30aad009099"}
+params = {:apikey => api_key}
 uri.query = URI.encode_www_form(params)
 
 res = Net::HTTP.get_response(uri)
--- storeObject.rb
+++ storeObject.rb
@@ -1,7 +1,8 @@
 require "net/http"
 require 'json'
 
+api_key= "58fa4fef-c49a-464e-82be-a30aad009099"
 uri = URI("https://api.idolondemand.com/1/api/async/storeobject/v1")
-#res = Net::HTTP.post_form(uri, 'file' => "thought.mp3", 'apikey' => "58fa4fef-c49a-464e-82be-a30aad009099")
-res = Net::HTTP.post_form(uri, 'url' => "http://downloads.bbc.co.uk/podcasts/radio4/thought/thought_20150302-1134a.mp3", 'apikey' => "58fa4fef-c49a-464e-82be-a30aad009099")
+#res = Net::HTTP.post_form(uri, 'file' => "thought.mp3", 'apikey' => api_key)
+res = Net::HTTP.post_form(uri, 'url' => "http://downloads.bbc.co.uk/podcasts/radio4/thought/thought_20150302-1134a.mp3", 'apikey' => api_key)
 puts res.body
