bd441ef73ac7cb2f76e9c8b693080f69a487fb73
djpidol <dominic.page@hp.com>
2015-04-17 13:51:15 +0200

    Update storeObject.rb
    changed API key

--- storeObject.rb
+++ storeObject.rb
@@ -2,6 +2,6 @@ require "net/http"
 require 'json'
 
 uri = URI("https://api.idolondemand.com/1/api/async/storeobject/v1")
-#res = Net::HTTP.post_form(uri, 'file' => "thought.mp3", 'apikey' => "3e25f657-9700-4e26-99ee-4c2544536e7a")
-res = Net::HTTP.post_form(uri, 'url' => "http://downloads.bbc.co.uk/podcasts/radio4/thought/thought_20150302-1134a.mp3", 'apikey' => "3e25f657-9700-4e26-99ee-4c2544536e7a")
+#res = Net::HTTP.post_form(uri, 'file' => "thought.mp3", 'apikey' => "58fa4fef-c49a-464e-82be-a30aad009099")
+res = Net::HTTP.post_form(uri, 'url' => "http://downloads.bbc.co.uk/podcasts/radio4/thought/thought_20150302-1134a.mp3", 'apikey' => "58fa4fef-c49a-464e-82be-a30aad009099")
 puts res.body
